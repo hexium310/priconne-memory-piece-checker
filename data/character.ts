3662d3f14e276a2d499411579ee9935af80249d3
Hexin <hexium310@gmail.com>
2019-09-18 22:50:46 +0900

    Update memory piece type of ネネカ

--- data/character.ts
+++ data/character.ts
@@ -635,7 +635,7 @@ export const characters: Character[] = [
     "initialRarity": 3,
     "maxRarity": 5,
     "hasUniqueEquipment": false,
-    "pieceType": "none",
+    "pieceType": "master",
   },
   {
     "name": "アオイ（編入生）",
