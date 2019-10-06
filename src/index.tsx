905cf2b77cf9aa339d85e0031e8038c354ceafbf
Hexin <hexium310@gmail.com>
2019-10-06 13:51:28 +0900

    Enable migration storage to v2

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
-
 import ReactDOM from 'react-dom';
 
+import { parseStorage, initStorage } from 'utils/storage/v2';
+import { migrateStorage, isStorageV2, OldStorage } from 'utils/storage/v2/migration';
 import { App } from 'components/App';
+
+const storage = parseStorage<OldStorage>();
+if (!isStorageV2(storage)) {
+  migrateStorage();
+}
+
+initStorage();
 
 ReactDOM.render(
   <App/>,
