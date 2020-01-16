30f480f38be8f9a3cb204b9be00b9bed86dca779
Hexin <hexium310@gmail.com>
2020-01-16 14:49:13 +0900

    Add new character

--- data/character.ts
+++ data/character.ts
@@ -721,4 +721,11 @@ export const characters: Character[] = [
     hasUniqueEquipment: false,
     pieceType: 'none',
   },
+  {
+    name: 'コッコロ（ニューイヤー）',
+    initialRarity: 3,
+    maxRarity: 5,
+    hasUniqueEquipment: false,
+    pieceType: 'none',
+  },
 ];
