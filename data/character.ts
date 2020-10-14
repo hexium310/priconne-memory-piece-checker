1129f280501c7212a8d0aea939bb72d2e1d737f0
Hexin <hexium310@gmail.com>
2020-10-15 02:08:39 +0900

    Add new character

--- data/character.ts
+++ data/character.ts
@@ -912,4 +912,11 @@ export const characters: Character[] = [
     hasUniqueEquipment: false,
     pieceType: ['none'],
   },
+  {
+    name: 'レイ（ハロウィン）',
+    initialRarity: 3,
+    maxRarity: 5,
+    hasUniqueEquipment: false,
+    pieceType: ['none'],
+  },
 ];
