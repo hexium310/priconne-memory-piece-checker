9eac686f6904ea02bb37cad98d541dd7aa1473f2
Hexin <hexium310@gmail.com>
2021-04-15 15:32:09 +0900

    Update piece type

--- data/character.ts
+++ data/character.ts
@@ -854,7 +854,7 @@ export const characters: Character[] = [
     initialRarity: 3,
     maxRarity: 5,
     hasUniqueEquipment: false,
-    pieceType: ['none'],
+    pieceType: ['hard'],
   },
   {
     name: 'ミサト（サマー）',
