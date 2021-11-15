f596905eec3f1293ab34626717b60b49b5e1408a
Hexin <hexium310@gmail.com>
2021-11-15 15:05:43 +0900

    Update piece types

--- data/character.ts
+++ data/character.ts
@@ -924,7 +924,7 @@ export const characters: Character[] = [
     initialRarity: 3,
     maxRarity: 5,
     hasUniqueEquipment: true,
-    pieceType: ['none'],
+    pieceType: ['hard'],
   },
   {
     name: 'トモ（マジカル）',
@@ -1197,7 +1197,7 @@ export const characters: Character[] = [
     initialRarity: 3,
     maxRarity: 5,
     hasUniqueEquipment: false,
-    pieceType: ['none'],
+    pieceType: ['master'],
   },
   {
     name: 'ホマレ',
